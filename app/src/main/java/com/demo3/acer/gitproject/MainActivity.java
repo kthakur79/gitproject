45576a9a3464df60c10863899ffc01c9c4a7e1e5
kthakur79 <saraalkshitijthakur@gmail.com>
2019-03-07 12:27:28 +0530

    added edittext and button

--- app/src/main/java/com/demo3/acer/gitproject/MainActivity.java
+++ app/src/main/java/com/demo3/acer/gitproject/MainActivity.java
@@ -2,12 +2,28 @@ package com.demo3.acer.gitproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et1;
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b=(Button)findViewById(R.id.button);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                et1=(EditText)findViewById(R.id.editText);
+                final String str=et1.getText().toString();
+                Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
